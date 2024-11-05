cefd143d8f476914c60f65edf799ab68a2bb0840
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-11-05 14:56:39 -0600

    Assignment finished

---
+++ LightingAndCamerasExample/CirclingCamera.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightingAndCamerasExample
+{
+    /// <summary>
+    /// A camera that circles the origin
+    /// </summary>
+    public class CirclingCamera : ICamera
+    {
+        /// <summary>
+        /// The camera's angle
+        /// </summary>
+        float angle;
+
+        /// <summary>
+        /// The camera's position
+        /// </summary>
+        Vector3 position;
+
+        /// <summary>
+        /// The camera's speed
+        /// </summary>
+        float speed;
+
+        /// <summary>
+        /// The game this camera belongs to
+        /// </summary>
+        Game game;
+
+        /// <summary>
+        /// The view matrix
+        /// </summary>
+        Matrix view;
+
+        /// <summary>
+        /// The projection matrix
+        /// </summary>
+        Matrix projection;
+
+        /// <summary>
+        /// The camera's view matrix
+        /// </summary>
+        public Matrix View => view;
+
+        /// <summary>
+        /// The camera's projection matrix
+        /// </summary>
+        public Matrix Projection => projection;
+
+
+        /// <summary>
+        /// Constructs a new camera that circles the origin
+        /// </summary>
+        /// <param name="game">The game this camera belongs to</param>
+        /// <param name="position">The initial position of the camera</param>
+        /// <param name="speed">The speed of the camera</param>
+        public CirclingCamera(Game game, Vector3 position, float speed)
+        {
+            this.game = game;
+            this.position = position;
+            this.speed = speed;
+            this.projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4,
+                game.GraphicsDevice.Viewport.AspectRatio,
+                1,
+                1000
+            );
+            this.view = Matrix.CreateLookAt(
+                position,
+                Vector3.Zero,
+                Vector3.Up
+            );
+        }
+
+        /// <summary>
+        /// Updates the camera's positon
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
+        public void Update(GameTime gameTime)
+        {
+            // update the angle based on the elapsed time and speed
+            angle += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Calculate a new view matrix
+            this.view =
+                Matrix.CreateRotationY(angle) *
+                Matrix.CreateLookAt(position, Vector3.Zero, Vector3.Up);
+        }
+
+    }
+}
---
+++ LightingAndCamerasExample/FPSCamera.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightingAndCamerasExample
+{
+    public class FPSCamera : ICamera
+    {
+        float horizontalAngle;
+
+        float verticalAngle;
+
+        Vector3 position;
+
+        MouseState oldMouseState;
+
+        Game game;
+
+        public Matrix View { get; protected set; }
+
+        public Matrix Projection { get; protected set; }
+
+        public float Sensitivity { get; set; } = 0.018f;
+
+        public float Speed { get; set; } = 0.5f;
+
+        public FPSCamera(Game game, Vector3 position)
+        {
+            this.game = game;
+            this.position = position;
+            this.horizontalAngle = 0;
+            this.verticalAngle = 0;
+            this.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, game.GraphicsDevice.Viewport.AspectRatio, 1, 1000);
+            Mouse.SetPosition(game.Window.ClientBounds.Width / 2, game.Window.ClientBounds.Height / 2);
+            oldMouseState = Mouse.GetState();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var keyboard = Keyboard.GetState();
+            var newMouseState = Mouse.GetState();
+
+            var facing = Vector3.Transform(Vector3.Forward, Matrix.CreateRotationY(horizontalAngle));
+
+            if (keyboard.IsKeyDown(Keys.W)) position -= facing * Speed;
+            if (keyboard.IsKeyDown(Keys.S)) position += facing * Speed;
+
+            if(keyboard.IsKeyDown(Keys.A)) position -= Vector3.Cross(Vector3.Up, facing) * Speed;
+            if (keyboard.IsKeyDown(Keys.D)) position += Vector3.Cross(Vector3.Up, facing) * Speed;
+
+            horizontalAngle += Sensitivity * (oldMouseState.X - newMouseState.X);
+            verticalAngle += Sensitivity * (oldMouseState.Y - newMouseState.Y);
+
+            Vector3 direction = Vector3.Transform(Vector3.Forward, Matrix.CreateRotationX(verticalAngle) * Matrix.CreateRotationY(horizontalAngle));
+
+            View = Matrix.CreateLookAt(position, position + direction, Vector3.Up);
+
+            Mouse.SetPosition(game.Window.ClientBounds.Width / 2, game.Window.ClientBounds.Height / 2);
+            oldMouseState = Mouse.GetState();
+        }
+
+
+    }
+}
